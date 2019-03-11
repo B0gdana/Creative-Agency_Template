5dca1ecb93e9233392e9d7043fc6f148f5d2ab0d
Bogdana Obukhivska <bogdana.obukhivska@gmail.com>
2019-03-11 15:00:08 +0000

    replace hex color names with css variables

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-document.querySelector('.login-form').addEventListener('submit', submitForm);
+document.querySelector('.contact-form').addEventListener('submit', submitForm);
 
 function submitForm(e) {
     e.preventDefault();
     fetch('form.php', {
         method: 'POST',
-        body: new FormData(document.querySelector('.login-form'))
+        body: new FormData(document.querySelector('.contact-form'))
     })
         .then( response => response.text() )
         .then( html => document.querySelector('.server-response')
