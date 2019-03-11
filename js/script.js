7fb7eb820e411f367f81ce0d82dc4202cfb2a027
Bogdana Obukhivska <bogdana.obukhivska@gmail.com>
2019-03-11 17:32:35 +0000

    place modal below content

--- js/script.js
+++ js/script.js
@@ -10,3 +10,18 @@ function submitForm(e) {
         .then( html => document.querySelector('.server-response')
                                 .innerHTML = html );
 }
+
+// const hideShow = document.getElementById('more-less-items');
+// document.getElementById('more-less-btn');
+// hideShow.addEventListener('click', showHideContent);
+// function showHideContent() {
+//     if (hideShow.style.display === 'none') {
+//         hideShow.style.display = 'flex';
+//        document.getElementById('more-less-btn').innerText = 'View less';
+        
+//     }
+//     else {
+//         hideShow.style.display = 'none';
+//         document.getElementById('more-less-btn').innerText = 'View all works';
+//     }
+// }
