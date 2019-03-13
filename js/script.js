6f2a19eded067d5580f33534a4e8624e7e3e6b1e
Bogdana Obukhivska <bogdana.obukhivska@gmail.com>
2019-03-13 17:21:55 +0000

    add smooth scrolling

--- js/script.js
+++ js/script.js
@@ -1,27 +1,41 @@
+$(function() {
+    $('.smooth').on('click', function(event) {
+        var target = $(this.getAttribute('href'));
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top - 125
+            }, 500);
+        }
+    });
+});
+
 document.querySelector('.contact-form').addEventListener('submit', submitForm);
 
 function submitForm(e) {
     e.preventDefault();
     fetch('form.php', {
-        method: 'POST',
-        body: new FormData(document.querySelector('.contact-form'))
-    })
-        .then( response => response.text() )
-        .then( html => document.querySelector('.server-response')
-                                .innerHTML = html );
+            method: 'POST',
+            body: new FormData(document.querySelector('.contact-form'))
+        })
+        .then(response => response.text())
+        .then(html => document.querySelector('.server-response')
+            .innerHTML = html);
 }
 
-// const hideShow = document.getElementById('more-less-items');
-// document.getElementById('more-less-btn');
-// hideShow.addEventListener('click', showHideContent);
-// function showHideContent() {
-//     if (hideShow.style.display === 'none') {
-//         hideShow.style.display = 'flex';
-//        document.getElementById('more-less-btn').innerText = 'View less';
-        
-//     }
-//     else {
-//         hideShow.style.display = 'none';
-//         document.getElementById('more-less-btn').innerText = 'View all works';
-//     }
-// }
+
+const hideShow = document.getElementById('more-less-items');
+document.getElementById('more-less-btn').addEventListener('click', showHideContent);
+
+function showHideContent() {
+    if (hideShow.getAttribute('class') === 'hidden') {
+        // hideShow.removeAttribute('class', 'hidden');
+        hideShow.setAttribute('class', 'visible');
+        document.getElementById('more-less-btn').innerHTML = 'View less';
+
+    }
+    else {
+        hideShow.setAttribute('class', 'hidden');
+        document.getElementById('more-less-btn').innerHTML = 'View all works';
+    }
+};
